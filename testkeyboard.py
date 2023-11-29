abb89ca76ad9b61c584cf56e03629f5f1668f945
ginloy <liyongleow@gmail.com>
2023-11-29 08:19:20 +0800

    improve keyboard scan script

--- testkeyboard.py
+++ testkeyboard.py
@@ -1,16 +1,25 @@
-import sys
-sys.path.append('..')
 import keyboard
 
+events = []
+
 def print_pressed_keys(e):
-    if e.event_type == "up":
-        return
     if e.name == "enter":
-        print("\r", end="", flush=True)
-    elif e.name == "space":
-        print(" ", end="", flush=True)
-    elif len(e.name) == 1:
-        print(e.name, end="", flush=True)
+        strings = keyboard.get_typed_strings(events)
+        for string in strings:
+            if len(string) == 0:
+                continue
+            print(string)
+        events.clear()
+    if len(events) == 0:
+        events.append(e)
+        return
+    last = events[-1]
+    # print(e.time - last.time)
+    if e.time - last.time < 0.015:
+        events.append(e)
+    else:
+        events.clear()
+        events.append(e)
 
 	
 keyboard.hook(print_pressed_keys)
