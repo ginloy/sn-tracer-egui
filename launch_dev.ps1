90f47c397f6553edfd4c8a4625123679a11796fe
ginloy <liyongleow@gmail.com>
2023-11-30 22:34:14 +0800

    fix powershell script

--- launch_dev.ps1
+++ launch_dev.ps1
@@ -1,10 +1,12 @@
 # Change directory to the directory of the script
 Set-Location $PSScriptRoot
 
-# Set environment variables
-$env:RUST_LOG = "debug"
-$env:SCANNER_PATH = "$(Get-Location)\target\release\scanner"
+powershell -Command {
+    # Set Environment Variables
+    $env:RUST_LOG = "debug"
+    $env:SCANNER_PATH = "$(Get-Location)\target\release\scanner"
 
-# Build and run the Rust code
-cargo build --release --bin scanner
-cargo run $args --bin sn-tracer-egui
+    # Run
+    cargo build --release --bin scanner
+    cargo run $args --bin sn-tracer-egui
+}
