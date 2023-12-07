ae25b3315d380b6726a16947d24e1fa658f10d6f
ginloy <liyongleow@gmail.com>
2023-12-07 17:01:24 +0800

    Add confirmation before cancel

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,20 @@ enum ConnectionStatus {
     Disconnected,
 }
 
+fn ask_confirmation(msg: &str) -> bool {
+    match MessageDialog::new()
+        .set_level(MessageLevel::Warning)
+        .set_title("Warning")
+        .set_description(msg)
+        .set_buttons(MessageButtons::OkCancel)
+        .show()
+    {
+        MessageDialogResult::Ok => true,
+        MessageDialogResult::Cancel => false,
+        _ => false,
+    }
+}
+
 impl App {
     // fn configure_text_styles(ctx: &egui::Context) {
     //     use FontFamily::Proportional;
@@ -282,7 +296,9 @@ impl eframe::App for App {
                     ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
                         let clear_button =
                             Button::new(RichText::new("Clear").heading()).fill(Color32::RED);
-                        if ui.add(clear_button).clicked() {
+                        if ui.add(clear_button).clicked()
+                            && ask_confirmation("Are you sure you want to clear all data?")
+                        {
                             self.barcode_input.clear();
                             self.device_output.clear();
                         };
